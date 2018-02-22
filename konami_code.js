805472ec825f8858ca021aa75b9571967cd79db5
samory harris <samory070@gmail.com>
2018-02-22 19:17:07 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -7,7 +7,13 @@ function onKeyDownHandler(e) {
   if(key === code[index]) {
     index++;
 
-    if(index === code.length)
+    if(index === code.length) {
+      alert('Hurray!');
+
+      index = 0;
+    }
+  } else {
+    index = 0;
   }
 }
 
